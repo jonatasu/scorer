8f3d0499072bb55cc7a934772e3cfedb874be1a2
Jônatas Filipe Vieira <jonatasvieira@gmail.com>
2022-01-07 19:32:07 -0300

    feat(app-bar): created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <div id="app">
+  <v-app id="app">
+    <app-bar></app-bar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
+<script>
+import AppBar from '@/components/AppBar.vue';
+
+export default {
+  components: {
+    AppBar,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
+body, #app {
+  min-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/AppBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-app-bar
+      fixed
+      elevation="2"
+      elevate-on-scroll
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>SCORER</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item @click="goTo('/')">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="goTo('/about')">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Sobre</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+  methods: {
+    goTo(route) {
+      this.$router.push(route);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
