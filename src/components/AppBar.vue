6d7b5ef53ec508e6df63f32f3b169b0cd2374bc3
Jônatas Filipe Vieira <jonatasvieira@gmail.com>
2022-01-09 18:48:30 -0300

    refactor(appbar): update
    Update the main item list and add the social media list items on the bottom of the drawer

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -5,7 +5,7 @@
       elevation="2"
       elevate-on-scroll
     >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = true" />
 
       <v-toolbar-title>SCORER</v-toolbar-title>
     </v-app-bar>
@@ -15,29 +15,91 @@
       fixed
       temporary
     >
-      <v-list
-        nav
-        dense
+      <v-layout
+        justify-space-between
+        column
+        fill-height
       >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item @click="goTo('/')">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
+        <div>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                SCORER
+              </v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
 
-          <v-list-item @click="goTo('/about')">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Sobre</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
+          <v-divider />
+
+          <v-list nav>
+            <v-list-item-group
+              v-model="group"
+              active-class="text--accent-4"
+            >
+              <v-list-item :to="{ name: 'Home' }">
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Início</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item :to="{ name: 'Home' }">
+                <v-list-item-icon>
+                  <v-icon>mdi-account</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Perfil</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item :to="{ name: 'Home' }">
+                <v-list-item-icon>
+                  <v-icon>mdi-shield-key</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Privacidade</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item :to="{ name: 'About' }">
+                <v-list-item-icon>
+                  <v-icon>mdi-information</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Sobre</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item href="mailto:scorer@gmail.com" target="_blank">
+                <v-list-item-icon>
+                  <v-icon>mdi-email-send</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Contato</v-list-item-title>
+              </v-list-item>
+
+              <v-divider />
+
+              <v-list-item dense>
+                <v-list-item-subtitle>Version 0.1.0</v-list-item-subtitle>
+              </v-list-item>
+
+            </v-list-item-group>
+          </v-list>
+        </div>
+        <v-footer>
+          <v-list class="d-flex justify-space-between social-media">
+            <v-list-item
+              v-for="item in socialMedia"
+              :key="item.icon"
+              class="px-0 mx-0 justify-center"
+            >
+              <v-btn
+                icon
+                :href="item.url"
+                target="_blank"
+              >
+                <v-icon size="24px">
+                  {{ item.icon }}
+                </v-icon>
+              </v-btn>
+            </v-list-item>
+          </v-list>
+          </v-footer>
+      </v-layout>
     </v-navigation-drawer>
   </div>
 </template>
@@ -47,6 +109,24 @@ export default {
   data: () => ({
     drawer: false,
     group: null,
+    socialMedia: [
+      {
+        icon: 'mdi-facebook',
+        url: 'https://www.facebook.com/Jownas87',
+      },
+      {
+        icon: 'mdi-twitter',
+        url: 'https://twitter.com/jownas87',
+      },
+      {
+        icon: 'mdi-youtube',
+        url: 'https://www.youtube.com/channel/UCv_aHAKZIJKPfKc9FCqS0Zw',
+      },
+      {
+        icon: 'mdi-instagram',
+        url: 'https://www.instagram.com/jownas1/',
+      },
+    ],
   }),
   methods: {
     goTo(route) {
@@ -56,6 +136,8 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.social-media {
+  flex: 1;
+}
 </style>
